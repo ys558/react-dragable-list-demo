b3cce977430d69830b5a776fade8a504b53d930a
ys558 <yuyi.gz@163.com>
2021-09-16 19:26:24 +0800

    update ro repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+// import { fabric } from "fabric";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+// import { render } from 'react-dom';
+// import { Stage, Layer, Rect, Text, Circle } from 'react-konva';
+// import Konva from 'konva';
+
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import '@atlaskit/css-reset'
+import styled from 'styled-components'
+
+const ColumnContainer = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`
+const ColumnTitle = styled.h3`
+  padding: 8px;
+`
+const ColumnTaskList = styled.div`
+  padding: 8px;
+`
+class Column extends Component {
+  render(){
+    return <DragDropContext
+        onDragEnd={result => {}}
+      >
+      <ColumnContainer>
+        <ColumnTitle>{this.props.column.title}</ColumnTitle>
+          <Droppable droppableId={this.props.column.id}>
+            { provided =>  <ColumnTaskList
+                innerRef={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {this.props.task.map(task => <Task key={task.id} task={task}/> )}
+                {provided.placeholder}
+              </ColumnTaskList>
+            }
+          </Droppable>
+      </ColumnContainer>
+    </DragDropContext>
+  }
 }
 
-export default App;
+
+const TaskContainer = styled.div`
+  padding: 8px;
+  border: 1px solid lightgrey;
+  margin-bottom: 8px;
+`
+class Task extends Component {
+  render(){
+    return <Draggable draggableId={this.props.task.id} index={this.props.index}>
+      <TaskContainer>
+        {this.props.task.content}
+      </TaskContainer>
+    </Draggable> 
+  }
+}
+
+const initialData = {
+  tasks: {
+    'task-1': {id: 'task-1', content: 'jjjjjpppp'},
+    'task-2': {id: 'task-2', content: 'xxxcc'},
+    'task-3': {id: 'task-3', content: 'yyyff'},
+    'task-4': {id: 'task-4', content: 'zzzeee'},
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+    }
+  },
+  columnOrder: ['column-1'],
+}
+
+export default class App extends Component {
+  state = initialData
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId]
+      const tasks = column.taskIds.map(taskIds => this.state.tasks[taskIds])
+      return <Column key={column.id} column={column} task={tasks}></Column>
+    })
+  }
+}
+
+
