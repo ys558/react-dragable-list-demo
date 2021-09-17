1abf2af18ef805832de820b333403f4fc3681ba7
ys558 <yuyi.gz@163.com>
2021-09-17 11:52:04 +0800

    update position persist in UI

---
+++ src/example-result.js
@@ -0,0 +1,10 @@
+const result = {
+  draggableId: 'task-1',
+  type: 'TYPE',
+  reason: 'DROP',
+  source: {
+    droppabledId: 'column-1',
+    index: 0
+  },
+  destination: null
+}
--- src/task.jsx
+++ src/task.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: white;
+`;
 
 export default class Task extends React.Component {
   render() {
     return (
       <Draggable draggableId={this.props.task.id} index={this.props.index}>
         {provided => (
-          <div style={{ border: '1px solid lightgrey', borderRadius: '2px', padding: '8px', marginBottom: '8px', background: 'white'}}
+          <Container style={{ border: '1px solid lightgrey', borderRadius: '2px', padding: '8px', marginBottom: '8px', background: 'white'}}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
             {this.props.task.content}
-          </div>
+          </Container>
         )}
       </Draggable>
     );
